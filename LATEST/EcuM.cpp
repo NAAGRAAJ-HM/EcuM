cacacd80f29fe50176bd8a0d05bce46f6c5df1f1
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:09:29 +0000

    Update: AUTOSAR basic software service module EcuM

--- LATEST/EcuM.cpp
+++ LATEST/EcuM.cpp
@@ -132,12 +132,10 @@ FUNC(void, ECUM_CODE) module_EcuM::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgEcuM;
          }
       }
