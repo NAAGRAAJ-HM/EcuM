8960767cad3f311fd9dfd079a4a9319f935e8ada
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:31:28 +0000

    Update: AUTOSAR basic software service module EcuM

--- LATEST/EcuM.cpp
+++ LATEST/EcuM.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgEcuM.hpp"
-#include "EcuM_core.hpp"
-#include "infEcuM_Exp.hpp"
+#include "EcuM.hpp"
 #include "infEcuM_Imp.hpp"
 
 /******************************************************************************/
@@ -32,34 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_EcuM:
-      INTERFACES_EXPORTED_ECUM
-   ,  public abstract_module
-   ,  public class_EcuM_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-
-   public:
-      FUNC(void, ECUM_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, ECUM_CONFIG_DATA, ECUM_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, ECUM_CODE) DeInitFunction           (void);
-      FUNC(void, ECUM_CODE) MainFunction             (void);
-      ECUM_CORE_FUNCTIONALITIES
-
-      FUNC(void, ECUM_CODE) InitFunction             (void);
-      FUNC(void, ECUM_CODE) StartupTwo               (void);
-      FUNC(void, ECUM_CODE) DeterminePbConfiguration (void);
-      FUNC(bool, ECUM_CODE) GetPendingWakeupEvents   (void);
-      FUNC(void, ECUM_CODE) GetValidatedWakeupEvents (void);
-      FUNC(void, ECUM_CODE) LoopDetection            (void);
-      FUNC(void, ECUM_CODE) SelectShutdownTarget     (void);
-};
-
-extern VAR(module_EcuM, ECUM_VAR) EcuM;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
